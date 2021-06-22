0d0c6939f139285e467f989bc354ca5634a33757
Ejulu <emox1995@gmail.com>
2021-06-22 22:46:31 +0300

    sent a get request to google

--- web-APIs/request.py
+++ web-APIs/request.py
@@ -1,1 +1,9 @@
-pass
+import requests
+def main():
+    response = requests.get("http://www.google.com")
+    print("STATUS CODE:", response.status_code)
+    print("HEADERS:", response.headers)
+    print("CONTENT:", response.text)
+
+if __name__ == '__main__':
+    main()

cb8a2328c742b4298780beb929496595466457f4
Ejulu <emox1995@gmail.com>
2021-06-23 22:42:57 +0300

    mad an api request

--- web-APIs/request.py
+++ web-APIs/request.py
@@ -1,9 +1,9 @@
 import requests
 def main():
-    response = requests.get("http://www.google.com")
+    response = requests.get("http://api.exchangeratesapi.io/latest")
     print("STATUS CODE:", response.status_code)
-    print("HEADERS:", response.headers)
-    print("CONTENT:", response.text)
+    print("content-type", response.headers["Content-Type"])
+ 
 
 if __name__ == '__main__':
     main()

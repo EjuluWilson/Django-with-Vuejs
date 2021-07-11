2d48366f687ef78ba56916072174bacd0f3fb369
Ejulu Wilson <emox1995@gmail.com>
2021-07-11 08:20:23 +0300

    isues fixed

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\projects\Django-with-Vuejs\venv
+set VIRTUAL_ENV=P:\Django-with-Vuejs\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
